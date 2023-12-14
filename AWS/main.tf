833ef4c2107a4e57062227ed2f8cdf7131b46ebc
Sravanthi chakravaram <sravanthich1305@gmail.com>
2023-12-14 12:42:16 -0500

    new variable shell script

---
+++ AWS/main.tf
@@ -0,0 +1,4 @@
+##VPC creation
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+}
---
+++ AWS/variable.tf
@@ -0,0 +1,17 @@
+variable "cidr" {
+  default = "var.cidr"
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "sub2" {
+  vpc_id = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
